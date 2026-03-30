3d73b44c7042620fc18ef3043e6c044b39ba1078
hunaa2006 <hunaahun0@gmail.com>
2026-03-30 23:27:57 +0800

    Add vision API config

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,29 @@ function main (fileNames: string[]): void {
 // Implement the async version of the above here
 // Your version should not use .then and should use try/catch instead of .catch
 async function mainAsync(fileNames: string[]): Promise<void> {
-    // Your code here
+    for (const fileName of fileNames) {
+        console.log(`Running logo detection on ${fileName}`);
+        try {
+            const [result] = await client.logoDetection(fileName);
+            let scores: number[] = [];
+            const logos = result.logoAnnotations;
+            logos?.forEach((logo) => {
+                if (logo.description)
+                    console.log(`"${logo.description}" found in in file ${fileName}`);
+                if (logo.score)
+                    scores.push(logo.score);
+            });
+            if (scores.length > 0) {
+                const avg = scores.reduce((a, b) => a + b) / scores.length;
+                console.log(`Average score for ${fileName}: ${avg}`);
+            }
+        } catch (err: any) {
+            if (err.code === 'ENOENT')
+                console.log(`File ${fileName} not found`);
+             else
+                console.log(`Error:`, err.message); 
+        }
+    }
 }
 
 main([
